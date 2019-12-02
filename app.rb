bec52699dd639a1655a8cff1f7a265e8e920e165
Jesse Byers <jesse.s.byers@gmail.com>
2019-12-02 20:23:30 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,17 +2,10 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
-# Create a dynamic route at get '/reversename/:name' that accepts a name
-# and renders the name backwards.
   get "/reversename/:name" do
     @name = params[:name]
     "#{@name.reverse}"
   end
-
-  # Create a dynamic route at get '/square/:number' that accepts a number and returns
-  # the square of that number.
-  # Note: Remember that values in params always come in as strings,
-  # and your return value for the route should also be a string (use .to_i and .to_s).
 
   get "/square/:number" do
     @num = params[:number]
