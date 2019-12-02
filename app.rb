23b8d43a8da0a99798e01384b1aa925595965dd2
Jesse Byers <jesse.s.byers@gmail.com>
2019-12-02 20:14:14 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,5 +2,11 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
+# Create a dynamic route at get '/reversename/:name' that accepts a name
+# and renders the name backwards.
+  get "/reversename/:name" do
+    @name = params[:name]
+    "#{@name.reverse}"
+  end
 
 end
