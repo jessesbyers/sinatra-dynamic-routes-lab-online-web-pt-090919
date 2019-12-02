218d7399a1d881b34ec9f1c68725c5390e942f12
Jesse Byers <jesse.s.byers@gmail.com>
2019-12-02 20:21:02 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -17,7 +17,17 @@ class App < Sinatra::Base
   get "/square/:number" do
     @num = params[:number]
     "#{(@num.to_i**2).to_s}"
+  end
 
+  # Create a dynamic route at get '/say/:number/:phrase' that accepts a number and a phrase
+  #  and returns that phrase in a single string the number of times given.
+
+  get "/say/:number/:phrase" do
+    @num = params[:number]
+    @phrase = params[:phrase]
+    @num.to_i times do
+      "#{@phrase}"
+    end
   end
 
 end
